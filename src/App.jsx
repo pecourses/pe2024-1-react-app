fb8989b06f319706055cf8944a6a6eaaf7476141
Viktoriia <freshitpte@gmail.com>
2024-07-13 15:48:31 +0300

    Add list

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
-import { useState } from "react";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-import Main from "./components/Main";
-
-const isLogin = true;
-const user = {
-  specOffers: "You have -20% for all laptops",
-};
-
 function App() {
-  const [isLight, setIsLight] = useState(true);
+  const numbers = [1, 2, 3, 4, 5];
 
-  const byttonStyle = {
-    color: isLight ? "yellow" : "grey",
-  };
+  const numbersList = numbers.map((n, index) => <li key={index}>{n}</li>);
 
-  //! move to className
-  const pageContainerStyle = {
-    minHeight: "100vh",
-    color: isLight ? "black" : "white",
-    backgroundColor: isLight ? "white" : "black",
-  };
-
-  function changeTheme() {
-    setIsLight(oldIsLight => !oldIsLight);
-  }
-
-  return (
-    <div style={pageContainerStyle}>
-      <Header isLogin={isLogin} />
-      <button onClick={changeTheme} style={byttonStyle}>
-        <i className="fa-solid fa-sun"></i>
-      </button>
-      <Main isLogin={isLogin} user={user} />
-      <Counter />
-    </div>
-  );
+  return <ul>{numbersList}</ul>;
 }
 
 export default App;
