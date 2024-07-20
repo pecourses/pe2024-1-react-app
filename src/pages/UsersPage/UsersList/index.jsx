78fd2c49e2d4453d89c3745dfd605da83b01ac4b
Viktoriia <freshitpte@gmail.com>
2024-07-20 20:25:28 +0300

    Rename currentPage to page

--- src/pages/UsersPage/UsersList/index.jsx
+++ src/pages/UsersPage/UsersList/index.jsx
@@ -7,48 +7,48 @@ function UsersList() {
   const [isFetching, setIsFetching] = useState(false); // чи іде завантаження
   const [error, setError] = useState(null); // помилка при завантаженні
   // TODO: rename to page
-  const [currentPage, setCurrentPage] = useState(1);
+  const [page, setPage] = useState(1);
   const [results, setResults] = useState(10);
 
   useEffect(() => {
     const savedPage = Number(window.localStorage.getItem("page"));
     if (savedPage) {
-      setCurrentPage(savedPage);
+      setPage(savedPage);
     }
   }, []);
 
   // працює без <React.StrictMode>
   useEffect(() => {
-    window.localStorage.setItem("page", currentPage);
-  }, [currentPage]);
+    window.localStorage.setItem("page", page);
+  }, [page]);
 
   useEffect(() => {
     setIsFetching(true);
     setError(null);
-    loadUsers({ page: currentPage, results })
+    loadUsers({ page, results })
       .then(({ results }) => setUsers(results))
       .catch(e => setError(e))
       .finally(() => setIsFetching(false));
-  }, [currentPage]);
+  }, [page]);
 
   const goPrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(page => page - 1);
+    if (page > 1) {
+      setPage(page => page - 1);
     }
   };
   const goNextPage = () => {
-    setCurrentPage(page => page + 1);
+    setPage(page => page + 1);
   };
 
   return (
     <>
       <div>
-        <button onClick={goPrevPage} disabled={currentPage === 1}>
+        <button onClick={goPrevPage} disabled={page === 1}>
           {"<"}
         </button>
-        <span>{currentPage}</span>
+        <span>{page}</span>
         <button onClick={goNextPage}>{">"}</button>
-      </div>{" "}
+      </div>
       {error && <div style={{ color: "red" }}>ERROR !!!</div>}
       {isFetching && <PacmanLoader color="green" speedMultiplier={1} />}
       <ul>
