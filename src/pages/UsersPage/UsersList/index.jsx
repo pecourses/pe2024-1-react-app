5dcf627c7e91a452f25086273631aba021af31f9
Viktoriia <freshitpte@gmail.com>
2024-07-20 18:13:12 +0300

    Add pages

--- src/pages/UsersPage/UsersList/index.jsx
+++ src/pages/UsersPage/UsersList/index.jsx
@@ -5,33 +5,54 @@ function UsersList() {
   const [users, setUsers] = useState([]);
   const [isFetching, setIsFetching] = useState(false); // чи іде завантаження
   const [error, setError] = useState(null); // помилка при завантаженні
+  const [currentPage, setCurrentPage] = useState(1);
+  const [results, setResults] = useState(10);
 
   useEffect(() => {
     setIsFetching(true);
     setError(null);
-    fetch("https://randomuser.me/api/?results=10")
+    fetch(
+      `https://randomuser.me/api/?page=${currentPage}&results=${results}&seed=pe2024`
+    )
       .then(response => response.json())
       .then(({ results }) => setUsers(results))
       .catch(e => setError(e))
       .finally(() => setIsFetching(false));
-  }, []);
+  }, [currentPage]);
+
+  const goPrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(page => page - 1);
+    }
+  };
+  const goNextPage = () => {
+    setCurrentPage(page => page + 1);
+  };
 
   return (
-    <ul>
-      {error && <div style={{ color: "red" }}>ERROR !!!</div>}
-      {isFetching && <PacmanLoader color="green" speedMultiplier={1} />}
-      {!error &&
-        !isFetching &&
-        users.map(u => (
-          <li key={u.login.uuid}>
-            {Object.keys(u).map(key => (
-              <p key={key}>
-                {key}: {JSON.stringify(u[key])}
-              </p>
-            ))}
-          </li>
-        ))}
-    </ul>
+    <>
+      <div>
+        <button onClick={goPrevPage} disabled={currentPage === 1}>
+          {"<"}
+        </button>
+        <button onClick={goNextPage}>{">"}</button>
+      </div>
+      <ul>
+        {error && <div style={{ color: "red" }}>ERROR !!!</div>}
+        {isFetching && <PacmanLoader color="green" speedMultiplier={1} />}
+        {!error &&
+          !isFetching &&
+          users.map(u => (
+            <li key={u.login.uuid}>
+              {Object.keys(u).map(key => (
+                <p key={key}>
+                  {key}: {JSON.stringify(u[key])}
+                </p>
+              ))}
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
 
