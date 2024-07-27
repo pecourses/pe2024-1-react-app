5542e8d7d7f85e0264f15584075015ddc7a1e8e7
Viktoriia <freshitpte@gmail.com>
2024-07-27 16:00:04 +0300

    Add context ex

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
-import StopWatch from "./components/StopWatch";
+import { createContext, useContext, useState } from "react";
+
+// 0 create context
+const DataContext = createContext("def data");
 
 function App() {
-  const [isHidden, setIsHidden] = useState(true);
+  const [data, setData] = useState("app data");
   return (
-    <>
-      <button onClick={() => setIsHidden(!isHidden)}>
-        {isHidden ? "Show" : "Hide"}
-      </button>
-      {!isHidden && <StopWatch />}
-    </>
+    // 1 обгорнути юерево компонентів в провайдер з пропом value
+    <DataContext.Provider value={data}>
+      <Child />
+    </DataContext.Provider>
   );
 }
 
 export default App;
+
+function Child() {
+  return <ChildChild />;
+}
+
+function ChildChild() {
+  // 2 зчитати контекст за іменем
+  const data = useContext(DataContext);
+
+  return <div>{data}</div>;
+}
+
+// App
+//   Child1
+//     ChildChild
+//   Child2
+//     ChildChild
