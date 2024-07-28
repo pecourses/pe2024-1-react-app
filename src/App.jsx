2970290ba2c6a0a704606af15b56c2fc8458e062
Viktoriia <freshitpte@gmail.com>
2024-07-28 16:43:37 +0300

    Add NotFOund? useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
 {
   /* <Route path="/about" Component={About} /> */
@@ -11,12 +18,35 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
 }
+// Link змінює location (host:port/location)
+// Router підписаний на зміну location
+// Коли location змінюєтиься, то Router шукає відповідний Route
+// і виводе з нього element / Component
 
 export default App;
+
+function NotFound() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const id = setTimeout(() => navigate("/"), 2000);
+    return () => clearTimeout(id);
+  });
+
+  return (
+    <div>
+      404 This page is not exists yet
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+    </div>
+  );
+}
 
 function Home() {
   return <div>Home</div>;
