aa888f196809bdac7623f3c4566c5296e9e9ac82
Viktoriia <freshitpte@gmail.com>
2024-07-28 19:03:45 +0300

    Add page changing

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -8,27 +8,46 @@ class UsersLoader extends Component {
       users: [],
       isFetching: false,
       error: null,
+      page: 1,
     };
   }
 
-  componentDidMount() {
+  loadUsers = () => {
     this.setState({ isFetching: true });
-    fetch(`https://randomuser.me/api?page=1&results=5&seed=js`)
+    fetch(`https://randomuser.me/api?page=${this.state.page}&results=5&seed=js`)
       .then(response => response.json())
       .then(data => this.setState({ users: data.results }))
       .catch(err => this.setState({ error: err }))
       .finally(() => this.setState({ isFetching: false }));
+  };
+
+  componentDidMount() {
+    this.loadUsers();
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page) {
+      this.loadUsers();
+    }
+  }
+
+  nextPage = () => this.setState({ page: this.state.page + 1 });
 
   render() {
     const { users, isFetching, error } = this.state;
     return (
       <div>
+        <button>prev</button>
+        <button onClick={this.nextPage}>next</button>
         {isFetching && <div>Loading... Please wait.</div>}
         {error && <div>ERROR</div>}
         {!isFetching &&
           !error &&
-          users.map(u => <li key={u.login.uuid}>{JSON.stringify(u)}</li>)}
+          users.map(u => (
+            <li key={u.login.uuid}>
+              {u.name.first} {u.name.last}
+            </li>
+          ))}
       </div>
     );
   }
