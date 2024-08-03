5dc5880487707cedc188963949d4d8e603f21d08
Viktoriia <freshitpte@gmail.com>
2024-08-03 20:02:52 +0300

    Add vslid/invalid input highlight

--- src/components/forms/ContactsForm/index.jsx
+++ src/components/forms/ContactsForm/index.jsx
@@ -1,4 +1,5 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
+import classNames from "classnames";
 import { CONTACTS_VALIDATION_SCHEMA } from "../../../utils/validationSchemas";
 import styles from "./ContactsForm.module.sass";
 
@@ -24,64 +25,99 @@ function ContactsForm() {
         onSubmit={handleSubmit}
         validationSchema={CONTACTS_VALIDATION_SCHEMA}
       >
-        {formikProps => (
-          <Form className={styles.form}>
-            <label className={styles.label}>
-              <span className={styles.inputCaption}>Name: </span>
-              <Field
-                className={styles.input}
-                type="text"
-                name="userName"
-                placeholder="User Name"
-                autoFocus
-              />
-              <ErrorMessage
-                className={styles.error}
-                name="userName"
-                component="span"
-              />
-            </label>
-            <label className={styles.label}>
-              <span className={styles.inputCaption}>Phone: </span>
-              <Field
-                className={styles.input}
-                type="text"
-                name="phoneNumber"
-                placeholder="+380XXXXXXXXX"
-              />
-              <ErrorMessage
-                className={styles.error}
-                name="phoneNumber"
-                component="span"
-              />
-            </label>
-            <label className={styles.label}>
-              <span className={styles.inputCaption}>Email: </span>
-              <Field
-                className={styles.input}
-                type="email"
-                name="email"
-                placeholder="your@mail"
-              />
-              <ErrorMessage
-                className={styles.error}
-                name="email"
-                component="span"
-              />
-            </label>
-            <label className={styles.label}>
-              <span className={styles.inputCaption}>Date of Birth: </span>
-              <Field className={styles.input} type="date" name="birthday" />
-              <ErrorMessage
-                className={styles.error}
-                name="birthday"
-                component="span"
-              />
-            </label>
-            <button type="submit">Save</button>
-            <button type="reset">Reset</button>
-          </Form>
-        )}
+        {formikProps => {
+          const userNameClassName = classNames(styles.input, {
+            [styles.valid]:
+              !formikProps.errors.userName && formikProps.touched.userName,
+            [styles.invalid]:
+              formikProps.errors.userName && formikProps.touched.userName,
+          });
+
+          const phoneClassName = classNames(styles.input, {
+            [styles.valid]:
+              !formikProps.errors.phoneNumber &&
+              formikProps.touched.phoneNumber,
+            [styles.invalid]:
+              formikProps.errors.phoneNumber && formikProps.touched.phoneNumber,
+          });
+
+          const emailClassName = classNames(styles.input, {
+            [styles.valid]:
+              !formikProps.errors.email && formikProps.touched.email,
+            [styles.invalid]:
+              formikProps.errors.email && formikProps.touched.email,
+          });
+
+          const birthdayClassName = classNames(styles.input, {
+            [styles.valid]:
+              !formikProps.errors.birthday && formikProps.touched.birthday,
+            [styles.invalid]:
+              formikProps.errors.birthday && formikProps.touched.birthday,
+          });
+
+          return (
+            <Form className={styles.form}>
+              <label className={styles.label}>
+                <span className={styles.inputCaption}>Name: </span>
+                <Field
+                  className={userNameClassName}
+                  type="text"
+                  name="userName"
+                  placeholder="User Name"
+                  autoFocus
+                />
+                <ErrorMessage
+                  className={styles.error}
+                  name="userName"
+                  component="span"
+                />
+              </label>
+              <label className={styles.label}>
+                <span className={styles.inputCaption}>Phone: </span>
+                <Field
+                  className={phoneClassName}
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="+380XXXXXXXXX"
+                />
+                <ErrorMessage
+                  className={styles.error}
+                  name="phoneNumber"
+                  component="span"
+                />
+              </label>
+              <label className={styles.label}>
+                <span className={styles.inputCaption}>Email: </span>
+                <Field
+                  className={emailClassName}
+                  type="email"
+                  name="email"
+                  placeholder="your@mail"
+                />
+                <ErrorMessage
+                  className={styles.error}
+                  name="email"
+                  component="span"
+                />
+              </label>
+              <label className={styles.label}>
+                <span className={styles.inputCaption}>Date of Birth: </span>
+                <Field
+                  className={birthdayClassName}
+                  type="date"
+                  name="birthday"
+                />
+                <ErrorMessage
+                  className={styles.error}
+                  name="birthday"
+                  component="span"
+                />
+              </label>
+              <button type="submit">Save</button>
+              <button type="reset">Reset</button>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
